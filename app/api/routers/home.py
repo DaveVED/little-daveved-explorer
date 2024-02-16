82b9a9b66654d000cb4a4144b567f39f390d578d
DaveVED <dave.w.dennis@gmail.com>
2024-02-16 00:22:23 -0600

    refactor url reidrect.

--- app/api/routers/home.py
+++ app/api/routers/home.py
@@ -1,16 +1,10 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 def world_map_root(request: Request):
-    context = {
-        "request": request,
-        "title": "Home"
-    }
-
-    response = templates.TemplateResponse("home.html", context)
-    return response
+    return RedirectResponse(url="/explorer")
